8297ab19ac07510d68c9b973d15061baa9a1bf8a
glitchy-cat <jimmybull18@gmail.com>
2022-01-13 21:05:02 -0500

    Completed challenge 4

--- loops-practice/script.js
+++ loops-practice/script.js
@@ -22,3 +22,30 @@ HINT: Call calcTip in the loop and use the push method to add values to the tips
 GOOD LUCK 😀
 */
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+
+  for(let i = 0; i < bills.length; i++) {
+    console.log(`The tip was ${calcTip(bills[i])}`);
+    tips.push(calcTip(bills[i]));
+    totals.push(tips[i] + bills[i]);
+  }
+  console.log(bills);
+  console.log(tips);
+  console.log(totals);
+
+  const calcAverage = function (arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        //sum = sum + arr[i]
+        sum += arr[i];
+    }
+        return sum / arr.length;
+}
+  console.log(calcAverage(totals));
+
