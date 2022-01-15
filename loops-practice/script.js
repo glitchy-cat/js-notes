0ec1c1e21ca83d4ac5e7d27bade93fde5d30b629
glitchy-cat <jimmybull18@gmail.com>
2022-01-15 01:21:16 -0500

    setup main

--- loops-practice/script.js
+++ loops-practice/script.js
@@ -27,25 +27,28 @@ const tips = [];
 const totals = [];
 
 const calcTip = function (bill) {
-   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  }
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-  for(let i = 0; i < bills.length; i++) {
-    console.log(`The tip was ${calcTip(bills[i])}`);
-    tips.push(calcTip(bills[i]));
-    totals.push(tips[i] + bills[i]);
-  }
-  console.log(bills);
-  console.log(tips);
-  console.log(totals);
-
-  const calcAverage = function (arr) {
-    let sum = 0
-    for (let i = 0; i < arr.length; i++) {
-        //sum = sum + arr[i]
-        sum += arr[i];
-    }
-        return sum / arr.length;
+for (let i = 0; i < bills.length; i++) {
+  console.log(`The tip was ${calcTip(bills[i])}`);
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
 }
-  console.log(calcAverage(totals));
+console.log(bills);
+console.log(tips);
+console.log(totals);
 
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //sum = sum + arr[i]
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));
+
+console.log('hello');
+console.log('Darkness my old friend');
+console.log('pls');
