ee7b0d85146882158eb6c7789650458cf3f0e6ff
glitchy-cat <jimmybull18@gmail.com>
2022-01-19 17:30:24 -0500

    Added course material folder

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+      if (js === "amazing")
+        alert("Javascript is FUN");
+        40+8+23-10;
+        consosle.log(40+8+23-10);
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/recap/script.js
@@ -0,0 +1,10 @@
+//Remember that from now on we'll use strict on all our scripts.
+'use strict';
+
+const x = 23;
+
+if (x > 2);
+
+console.log('yes');
+
+console.log('no');
