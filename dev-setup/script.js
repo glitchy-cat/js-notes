0e871cb69f2c5345c573ae2ff5c03ba3a71a0dea
glitchy-cat <jimmybull18@gmail.com>
2022-01-19 16:57:03 -0500

    Completed debugger tutprial

--- dev-setup/script.js
+++ dev-setup/script.js
@@ -62,11 +62,13 @@ const measureKelvin = () => {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
-    value: Number(prompt('Degrees Celsius')), //Prompt always returns a string so its producing a bug
+
+    // C) FIX
+    //value: Number(prompt('Degrees Celsius')), //Prompt always returns a string so its producing a bug
   };
 
-  console.log(measurement);
-  console.table(measurement)
+  // B) Find the bug
+  console.table(measurement);
 
   //console.warn(measurement.value);
   //console.error(measurement.value);
@@ -75,5 +77,32 @@ const measureKelvin = () => {
   return kelvin;
 };
 // A) Identify the bug
-// — bug occurs on 65
+// — bug occurs on 67
 console.log(measureKelvin());
+
+////////////////////////////////////////////////////////
+
+//Using a debugger
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+// A) Identify Bug
+console.log(amplitudeBug);
